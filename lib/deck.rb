b1c7ec1af88a0a82a839ee6014881b310a429006
Kameron Johnson <johnson.10196@osu.edu>
2026-05-28 15:39:39 -0400

    initial commit - Initial commit with project files

---
+++ lib/deck.rb
@@ -0,0 +1,7 @@
+#Created 5/24/2026 by Kameron Johnson
+#Waiting for teammate to implement Deck class
+class Deck
+  def shuffle
+    # wait for teammate
+  end
+end
---
+++ lib/set_validator.rb
@@ -0,0 +1,4 @@
+#Created 5/24/2026 by Kameron Johnson
+#Waiting for teammate to implement SetValidator class
+class SetValidator
+end
