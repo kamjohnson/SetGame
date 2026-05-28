b627568b7885ac6df5264cdba574a3f35a0ea4c7
Hongle Chen <chen.12203@buckeyemail.osu.edu>
2026-05-28 23:44:32 +0000

    update card and deck class, create rspec tests for both class

---
+++ spec/deck_spec.rb
@@ -0,0 +1,25 @@
+
+# create 5/27 by Hongle Chen
+# This file did test for checking if the deck has 81 cards and if there are no duplicate cards in the deck
+require 'deck'
+# Created 5/27  By Hongle Chen
+describe 'deckHas81Cards' do
+    it 'Return true if deck has 81 cards' do
+        deck = Deck.new
+        output = (deck.cards.length == 81)
+        expect(output).to be_truthy
+    end
+end
+# Created 5/27  By Hongle Chen
+describe 'deckHasNoDuplicateCards' do
+    it 'Return true if deck has no duplicate cards' do
+        deck = Deck.new
+
+        signatures = deck.cards.map do |card|
+            [card.shape, card.color, card.number, card.pattern]
+        end
+
+        output = (signatures.uniq.length == signatures.length)
+        expect(output).to be_truthy
+    end
+end
