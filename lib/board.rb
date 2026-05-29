16225430f3c4af9713d699ec9fc0790bcdd1b077
DenistheZ <115365066+DenistheZ@users.noreply.github.com>
2026-05-28 22:09:51 -0400

    Improved Board display
    Enhance board display and add card selection method.

--- lib/board.rb
+++ lib/board.rb
@@ -10,10 +10,17 @@ class Board
   end 
 
   def display_board
-    @visible_cards.each do |card|
-      puts card.to_s
+        puts "\n - Current Board -"
+        @visible_cards.each_with_index do |card, index|
+            puts "#{index +1}: #{card}"
+        end
     end
     
-    # have more def to add regarding actually replacing cards
-    # and locking the cards so other player can't select
+    def get_selection
+        puts "\nEnter the numbers of 3 cards you want to select (e.g., 1, 2, 3):"
+        input = gets.chomp
+
+        selected_cards = input.split(',').map {|num| num.strip.to_i - 1}
+        return selected_cards
+    end
 end
