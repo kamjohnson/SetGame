9582e7623ab2db50f1234f3df23a80884fdb91b2
mcintron0 <cintron.30@buckeyemail.osu.edu>
2026-05-31 10:47:11 -0400

    moved shuffle! call

--- lib/game_environment.rb
+++ lib/game_environment.rb
@@ -1,18 +1,24 @@
-# Created 5/24/2026 by Kameron Johnson
-# Edited 5/25/2026 - Added #initialize, #start_game, and #quit_game methods
-# Edited 5/26/2026 - Added #pause_game method
-# Edited 5/27/2026 - Added #handle_pause_selection and #pause_action methods
-# Edited 5/28/2026 - Fixed bug in #pause_action returning :nil symbol instead of nil
+=begin
+Created 5/24/2026 by Kameron Johnson
+Edited 5/25/2026 - Added #initialize, #start_game, and #quit_game methods
+Edited 5/26/2026 - Added #pause_game method
+Edited 5/27/2026 - Added #handle_pause_selection and #pause_action methods
+Edited 5/28/2026 - Fixed bug in #pause_action returning :nil symbol instead of nil
+Edited 5/31/26 by Michael Cintron - Moved @deck.shuffle! call to initialize
+=end
 require_relative 'deck'
 require_relative 'board'
 require_relative 'set_validator'
 class GameEnvironment
 
   # Created 5/25/2026 by Kameron Johnson
+  # Modified 5/31/26 by Michael Cintron - moved deck shuffling here so the board gets
+  #   the shuffled deck
   # Runs automatically when a new GameEnvironment object is created
   attr_reader :state, :mode
   def initialize
     @deck = Deck.new
+    @deck.shuffle!
     @board = Board.new @deck
     @players = []
     @validator = SetValidator.new
@@ -21,13 +27,13 @@ class GameEnvironment
 
   # Created 5/25/2026 by Kameron Johnson
   # Modified 5/28/2026 by Michael Cintron - corrected shuffle! call
+  # Modified 5/31/26 by Michael Cintron - Moved shuffle! call to initialize
   # Starts the game by shuffling the deck and setting up the board, changes state to midgame.
   def start_game
     return if @state == :midgame
     @state = :midgame
-    @deck.shuffle!    
-    #@board.setup(@deck)  # this method does not exist
     while @state == :midgame
+      @board.display_board
       puts "Drawing 3 cards..."
       cards = @deck.draw(3)
       puts cards
