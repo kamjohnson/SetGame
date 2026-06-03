84879a7e3ed52a2b72921c1b3feeebcbd4c9086b
HongleChen <chen.12203@buckeyemail.osu.edu>
2026-06-02 22:48:06 -0400

    change to terse code for deck and card.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,19 +11,15 @@ class Deck
         # This line by using product to create 81 unique cards, product is 3x3x3x3,which is use each elements to create cards, and none of it is duplicate
         # The terse code map put the result from product into card.new, to create 81 cards
         @cards = Card::SHAPE.product(Card::COLOR, Card::NUMBER,Card::PATTERN).map {|shape, color, number, pattern| Card.new(shape, color, number, pattern) }
-            
-        
-        
     end
     # Created 5/27  By Hongle Chen
-    def shuffle!
-        @cards.shuffle!
-    end
+    # into one line terse code 6/2/26 Hongle Chen
+    def shuffle! = @cards.shuffle!
     # Created 5/27  By Hongle Chen
-    def draw(number)
-        @cards.shift(number)
-    end
+    # into one line terse code 6/2/26 Hongle Chen
+    def draw(number) = @cards.shift(number)
 end
+
 
 
 
