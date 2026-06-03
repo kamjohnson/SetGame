67c42b768b4b3606a04edcf8f1d2a68902b5de78
mcintron0 <cintron.30@buckeyemail.osu.edu>
2026-06-02 22:36:48 -0400

    small documentation addition

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,6 +3,7 @@
 # This file did test for checking if the deck has 81 cards and if there are no duplicate cards in the deck
 require 'deck'
 # Created 5/27  By Hongle Chen
+# Modified 6/2/26 by Michael Cintron - now using the card_count method
 describe 'deckHas81Cards' do
     it 'Return true if deck has 81 cards' do
         deck = Deck.new
